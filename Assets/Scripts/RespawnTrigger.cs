923415588e4fbb634ddbb0674dd47a2a8654476b
SyL <sobbyetsy@gmail.com>
2026-05-05 09:54:17 +0200

    respawn particles

--- Assets/Scripts/RespawnTrigger.cs
+++ Assets/Scripts/RespawnTrigger.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class RespawnTrigger : MonoBehaviour
 {
     [SerializeField] private Transform respawnPoint;
+    [SerializeField] private GameObject respawnPoofPrefab; // ← ADD
 
     private void OnTriggerEnter(Collider other)
     {
         CharacterController cc = other.gameObject.GetComponent<CharacterController>();
-
         if (cc != null)
         {
             Respawn(cc);
@@ -16,8 +16,22 @@ public class RespawnTrigger : MonoBehaviour
 
     private void Respawn(CharacterController cc)
     {
+        // ← POOF at death position FIRST
+        if (respawnPoofPrefab != null)
+        {
+            GameObject poof = Instantiate(respawnPoofPrefab, cc.transform.position, Quaternion.identity);
+            Destroy(poof, 1f);
+        }
+
         cc.enabled = false;
-        cc.transform.position = this.respawnPoint.position;
+        cc.transform.position = respawnPoint.position;
         cc.enabled = true;
+
+        // ← POOF at respawn position too
+        if (respawnPoofPrefab != null)
+        {
+            GameObject poof = Instantiate(respawnPoofPrefab, respawnPoint.position, Quaternion.identity);
+            Destroy(poof, 1f);
+        }
     }
 }
